cc7ac91ecd633ff77892b3e71bb2a216e29f961e
Oleksandr Kondratiuk <48170652+alex-kondr@users.noreply.github.com>
2024-02-24 07:22:26 +0200

    save

---
+++ GoITeens_UA_PYTHON_1y_4_24_02_24/1/test.py
@@ -0,0 +1,12 @@
+print("Hello")
+print("Hello world")
+print()
+
+name = input("What is your name? ")
+
+print("My name is", name)
+print(f"My name is {name}")
+print("My name is {}".format(name))
+
+name = input("What is your name? ")
+print(name)
