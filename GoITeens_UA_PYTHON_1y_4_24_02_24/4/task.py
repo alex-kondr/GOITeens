e3dbbf69c45f656d8a0b0b7c98ea666df5f4c421
Oleksandr Kondratiuk <48170652+alex-kondr@users.noreply.github.com>
2024-03-17 11:40:21 +0200

    save

--- GoITeens_UA_PYTHON_1y_4_24_02_24/4/task.py
+++ GoITeens_UA_PYTHON_1y_4_24_02_24/4/task.py
@@ -138,6 +138,19 @@
 # Якщо число більше 20, то вивести його на екран.
 # Якщо число більше 100 — закінчити роботу програми.
 
+# while True:
+#     number = float(input("Введіть своє число: "))
+
+#     if number > 100:
+#         print("Bye")
+#         break
+
+#     elif number > 20:
+#         print(number)
+
+
+
+
 
 
 
@@ -148,11 +161,13 @@
 # while True:
 #     number = input("number = ")
 #     number = int(number)
+
 #     while True:
 #         print(number)
 #         number = number - 1
 #         if number < 0:
 #             break
+#     break
 
 
 
@@ -162,6 +177,19 @@
 # Порахувати середнє арифметичне всіх чисел кратних 5 з певного інтервалу
 # start = int(input("Початок діапазону > "))
 # end = int(input("Кінець діапазону > "))
+
+# sum = 0
+# count = 0
+
+# for i in range(start, end + 1):
+#     if not i % 5:
+#         sum += i
+#         count += 1
+
+# result = sum / count
+
+# print(f"Середнє арифметичне чисел від {start} до {end} дорівнює {result}")
+
 
 
 
@@ -206,17 +234,37 @@
 
 # Дано ціле число, що лежить у діапазоні 1-999.
 # Вивести рядок-опис виду «парне двозначне число», «непарне тризначне число» тощо.
+# while True:
+#     a = int(input("Введіть ціле число: "))
 
+#     is_even = False
+#     count_digit = 0
 
+#     if a % 2 == 0:
+#         is_even = True
 
+#     if a // 100:
+#         count_digit = 3
+#     elif a // 10:
+#         count_digit = 2
+#     else:
+#         count_digit = 1
 
+#     print(f"Число {a} {'парне' if is_even else 'непарне'} та має таку кількість цифр: {count_digit}")
 
 
 
 
 # Написати код який буде просити команду і відразу її друкувати
 # до поки він його не напише "stop", "exit" aбо "bye".
+# while True:
+#     command = input("Введіть свою команди: ")
 
+#     if command == "stop" or command == "exit" or command == "bye":
+#         print("Bye bye")
+#         break
+
+#     print(f"{command = }")
 
 
 
@@ -226,6 +274,16 @@
 
 
 # Написати код який рахує добуток всіх чисел в діапазоні який задав користувач
+# start = int(input("Введіть ціле число для початоку діапазону: "))
+# end = int(input("Введіть ціле число для кінця діапазону: "))
+
+# result = 1
+
+# for num in range(start, end + 1):
+#     result *= num
+
+# print(f"Добуток чисел від {start} до {end} дорівнює {result}")
+
 
 
 
@@ -239,16 +297,22 @@
 # Надайте змінній access значення, яке покаже, чи є доступ у користувача. Використовуйте логічні оператори.
 # Адміністратор завжди має доступ, незалежно від значень змінних is_permission та is_active.
 # Користувач має доступ, тільки якщо is_permission дорівнює True та is_active також дорівнює True.
+# is_active = True
+# is_admin = False
+# is_permission = True
 
-
-
-
+# access = "Доступ дозволено" if is_admin or (is_active and is_permission) else "Доступ відхилено"
+# print(f"{access}")
 
 
 
 
 # Написати програму, яка виведе список квадратів чисел.
+# start = 1
+# end = 100
 
+# for i in range(start, end + 1):
+#     print(i ** 2)
 
 
 
@@ -261,6 +325,12 @@
 
 
 # Написати програму, яка виведе числа від 7 до 77 використовуючи цикл
+# start = 7
+# end = 77
+
+# for i in range(start, end + 1):
+#     print(f"{i = }")
+
 
 
 
@@ -274,8 +344,14 @@
 # понад 5 років — це Сеньйор розробник, а від одного року до 5 включно — мідл.
 # Є змінна work_experience, що визначає стаж роботи програміста.
 # Залежно від стажу роботи, присвоїти змінній developer_type значення "Junior", "Middle" або "Senior".
+# work_experience = 4
 
-
+# if work_experience > 5:
+#     print("Senior")
+# elif work_experience <= 1:
+#     print("Junior")
+# else:
+#     print("Middle")
 
 
 
@@ -283,7 +359,11 @@
 
 
 # Дано змінну a = -56. Виводити a, поки a <= 15, збільшуючи змінну на 3
+# a = -56
 
+# while a <= 16:
+#     print(f"{a = }")
+#     a += 3
 
 
 
@@ -296,9 +376,12 @@
 
 
 # Написати програму, яка на початку додасть “I am glad to see you, ” до кожного із імен в списку
-# і виведе привітання на екран.
-# lst = ["Alex", "Petro", "Max", "Andrii", "Lev"]. Приклад — I am glad to see you, Alex!
+# і виведе привітання на екран.Приклад — I am glad to see you, Alex!
 
+# names = ["Alex", "Petro", "Max", "Andrii", "Lev"]
+
+# for name in names:
+#     print(f"\nI am glad to see you, {name}!", end="\n\n")
 
 
 
@@ -313,7 +396,19 @@
 # Корінь рівняння помістіть у змінні x1 та x2
 # x1 = (-b + D ** 0.5) / (2 · a)
 # x2 = (-b - D ** 0.5) / (2 · a)
+# a = 1
+# b = 5
+# c = 9
 
+# D = b ** 2 - 4 * a * c
+# if D >= 0:
+#     x1 = (-b + D ** 0.5) / (2 * a)
+#     x2 = (-b - D ** 0.5) / (2 * a)
+
+#     print(f"{x1 = }, {x2 = }")
+
+# else:
+#     print("Розв'язку немає")
 
 
 
@@ -322,7 +417,15 @@
 
 
 # Дано слово "Молоко". За допомогою циклу порахувати скільки літер "о" містить в собі це слово.
+# string = "Молокоoooooooooooooooooo"
 
+# count = 0
+
+# for char in string:
+#     if char == "o":
+#         count += 1
+
+# print(f"Кількість букв 'о' в слові {string} дорвінює {count}")
 
 
 
@@ -331,8 +434,16 @@
 
 # Зробити попередню програму більш універсальною:
 # Запитувати у користувача речення та літеру для пошуку.
+# string = input("Введіть своє речення > ")
+# find_char = input("Введіть символ для пошуку > ")
 
+# count = 0
 
+# for char in string:
+#     if char == find_char:
+#         count += 1
+
+# print(f"Кількість символів '{find_char}' в речені '{string}' дорвінює {count}")
 
 
 
