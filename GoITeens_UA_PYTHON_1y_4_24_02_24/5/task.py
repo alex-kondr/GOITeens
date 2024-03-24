7d8317a5afac7afc25639c2a47f6e8188493ad64
Oleksandr Kondratiuk <48170652+alex-kondr@users.noreply.github.com>
2024-03-24 11:46:49 +0200

    save

--- GoITeens_UA_PYTHON_1y_4_24_02_24/5/task.py
+++ GoITeens_UA_PYTHON_1y_4_24_02_24/5/task.py
@@ -92,7 +92,7 @@
 # my_list_2 = []
 
 # for i in my_list:
-#     my_list_2.append(i ** 2)
+#     my_list_2.append(i ** 4)
 
 # print(my_list_2)
 
@@ -114,9 +114,14 @@
 
 
 
+# products = ["Хліб", "Банани", "Риба", "Банани", "Насіння", "Майонез", "Кетчуп", "Банани", "Соус"]
+# products_2 = products
+
+# print(products[::3])
 
 
-# Написати програму, яка порахує і виведе числа у квадраті від 1 до числа, яке задасть користувач
+
+
 
 
 
@@ -125,6 +130,18 @@
 
 
 # Написати програму, що прибере дублікати зі списку.
+# products = ["Хліб", "Банани", "Риба", "Банани", "Насіння", "Майонез", "Кетчуп", "Банани", "Соус", "Кетчуп"]
+# products_copy = products.copy()
+# products_clear = []
+
+# for product in products:
+#     while product in products_copy:
+#         products_copy.remove(product)
+
+#     if product not in products_clear:
+#         products_clear.append(product)
+
+# print(products_clear)
 
 
 
@@ -132,7 +149,20 @@
 
 
 # Дано дійсне число - ціна 3 кг сала. Вивести вартість 1.2, 1.4, ..., 2 кг сала.
+# price = 450
+# a = 3
 
+# price_one = price / a
+# price_list = []
+
+# b = 1.2
+
+# while b <= 2:
+#     new_price = round(price_one * b, 0)
+#     price_list.append(new_price)
+#     b += 0.2
+
+# print(f"{price_list = }")
 
 
 
@@ -140,28 +170,85 @@
 
 
 # Написати програму, що порахує кількість додатніх елементів у списку та кількість від’ємних
+# list_numbers = []
+# for i in range(-10, 20, 3):
+#     list_numbers.append(i**3)
+
+# list_numbers = [i for i in range(-10, 20, 3)]
+# count = 0
+# pos = [count+1 for i in range(-10, 20, 3) if i > 0]
+# print(sum(pos))
+# neg = [count+1 for i in list_numbers if i < 0]
+# print(sum(neg))
+# for i in list_numbers:
+#     if i > 0:
+#         count_pos += 1
+#     elif i < 0:
+#         count_neg += 1
+
+# print(f"Кількість додатніх чисел: {count_pos}\nК-ть негативних: {count_neg}")
 
 
+# products = ["Хліб", "Банани", "Риба", "Банани", "Насіння", "Майонез", "Кетчуп", "Банани", "Соус", "Кетчуп"]
+# products_2 = [product for product in products if product == "Банани"]
+
+# products_2 = []
+# for product in products:
+#     if product == "Банани":
+#         products.append(product)
+
+
+# print(products_2)
 
 
 
 
 
 # Дано діапазон чисел від -100 до 0. Вивести на екран лише числа, які закінчуються на нуль
+# list_numbers = [i for i in range(-100, 1) if i % 10 == 0]
+# print(list_numbers)
+
+# for i in range(-100, 1):
+#     if i % 10 == 0:
+#         print(i)
+
+
 
 
 
 
 
 # Написати програму, що порахує кількість унікальних елементів у списку.
+# products = ["Хліб", "Банани", "Риба", "Банани", "Насіння", "Майонез", "Кетчуп", "Банани", "Соус", "Кетчуп"]
+# products_new = []
 
+# for product in products:
+#     if product not in products_new:
+#         products_new.append(product)
+
+# print(len(products_new))
 
 
 
 
 
 # Написати програму, яка зчитає 3 числа (a, b, c) та порахує скільки чисел лежить між “a” i “b”, які діляться на “с”
+# a = int(input("Введіть початок діапаону: "))
+# b = int(input("Введіть кінець діапазону: "))
+# c = int(input("Введіть число на яке потрібно ділити: "))
+# count = sum([1 for i in range(a, b+1) if i % c == 0])
 
+# # count = 0
+
+# # for i in range(a, b + 1):
+# #     if i % 4 == 0:
+# #         count += 1
+
+# print(f"Кількість чисел, які діляться на {c} в діапазоні від {a} до {b} дорівнює {count}")
+
+# my_list = [2, 8.9, -5, 9.7, 15]
+# sum_my_list = sum(my_list)
+# print(round(sum_my_list, 9))
 
 
 
@@ -169,17 +256,51 @@
 
 
 # Написати програму, що виведе список, після видалення із нього парних елементів
+# list_numbers = []
+# for i in range(-1000, 1000):
+#     if i % 2:
+#         list_numbers.append(i)
+
+
+# list_numbers = [i for i in range(-1000, 1000) if i % 2]
+
+
+
+
+
+
 
 
 
 
 
 # Створіть програму, яка приймає список чисел та виводить на екран кожне третє число у списку.
+# list_numbers = []
+
+# while True:
+#     number = input("Введіть число або 0: ")
+
+#     if number == "0":
+#         break
+
+#     if number.isdigit():
+#         list_numbers.append(float(number))
+#     else:
+#         print("Це не число")
+
+# for i in range(0, len(list_numbers), 3):
+#     print(list_numbers[i])
+
+
 
 
 
 
 # Написати програму, що виведе список, після видалення із нього чисел, які закінчуються на нуль
+# list_numbers = [i for i in range(100)]
+
+# list_numbers_new = [i for i in list_numbers if i % 10]
+# print(list_numbers_new)
 
 
 
