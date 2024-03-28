93510b22b645d30e04b9e4b3d6c7b5bfbc30dba1
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-03-28 16:29:27 +0200

    save

--- Заняття 6. Методи списків.py
+++ Заняття 6. Методи списків.py
@@ -22,7 +22,7 @@
 
 # random.randint(15, 60)
 
-prods = [
+products = [
     "Гречка",
     "Макарони",
     "Спагеті",
@@ -44,41 +44,138 @@ prods = [
     "Чай",
     "Кава"
 ]
-
-products = []
-print(["№", "Назва товару"])
-for i, product in enumerate(prods):
-    print([i+1, product])
-    products.append([i+1, product])
+products_sold = []
 
 commands = [
-    "Показати список наявних товарів",
-    "Додати новий товар до списку",
-    "Додати список товарів",
-    "Видалити товар за ім'ям",
-    "Видалити товар за номер",
-    "Відсортувати список товарів за ім'ям товарів",
-    "Відсортувати список товарів за номером"
+    "1. Показати список наявних товарів",
+    "2. Додати новий товар до списку",
+    "3. Додати список товарів",
+    "4. Видалити товар за ім'ям",
+    "5. Видалити товар за номер",
+    "6. Відсортувати список товарів за ім'ям",
+    "7. Продати товар",
+    "8. Знайти номер товару за ім'ям",
+    "9. Показати список проданих товарів",
+    "10. Показати історію продажів",
+    "11. Вийти з програми"
 ]
 
 while True:
-    break
+    print("Введіть номер команди")
+    for command in commands:
+        print(command)
 
-stop_worlds = ["stop", "exit"]
-while True:
-    print(f"Введіть назву товару для додавання у список товарів або введіть одне зі слів {stop_worlds} для припинення")
-    product = input()
+    command = input("-> ")
 
-    if product in stop_worlds:
+    if not command.isdigit():
+        print("Не відома команда")
+        input("Натисніть 'Enter' для продовження")
+        continue
+
+    if command == "1":
+        for i in range(len(products)):
+            print(f"{i + 1}. {products[i]}")
+
+        input("Натисніть 'Enter' для продовження")
+
+    elif command == "2":
+        product = input("Введіть назву товару: ")
+
+        if product not in products:
+            products.append(product)
+            print(f"Товар '{product}' додано до списку")
+        else:
+            print("Такий товар вже наявний у списку")
+
+        input("Натисніть 'Enter' для продовження")
+
+    elif command == "3":
+        prods = input("Введіть список товарів через пробіл:\n")
+        prods = prods.split()
+        products.extend(prods)
+        input("Натисніть 'Enter' для продовження")
+
+    elif command == "4":
+        product = input("Введіть назву товару для видалення: ")
+        if product in products:
+            products.remove(product)
+        else:
+            print("Такого товару немає у списку")
+
+        input("Натисніть 'Enter' для продовження")
+
+    elif command == "5":
+        product_index = input("Введіть номер товару, щоб видалити: ")
+        if not product_index.isdigit():
+            print("Це не номер")
+            input("Натисніть 'Enter' для продовження")
+            continue
+
+        product_index = int(product_index) - 1
+        if not(0 <= product_index < len(products)):
+            print("Такого номеру товару немає у списку")
+            input("Натисніть 'Enter' для продовження")
+            continue
+
+        product = products.pop(product_index)
+        print(f"Товар '{product}' видалено")
+        input("Натисніть 'Enter' для продовження")
+
+    elif command == "6":
+        products.sort()
+        print("Список відсортовано")
+        input("Натисніть 'Enter' для продовження")
+
+    elif command == "7":
+        product = input("Введіть назву товару для продажу: ")
+
+        if product not in products:
+            print("Такого товару немає")
+            input("Натисніть 'Enter' для продовження")
+            continue
+
+        products.remove(product)
+        products_sold.append(product)
+        input("Натисніть 'Enter' для продовження")
+
+    elif command == "8":
+        product = input("Введіть назву товару для пошуку: ")
+
+        if product not in products:
+            print("Такого товару немає")
+            input("Натисніть 'Enter' для продовження")
+            continue
+
+        product_index = products.index(product)
+        print(f"Товар '{product}' знаходиться під номером '{product_index + 1}'")
+        input("Натисніть 'Enter' для продовження")
+
+    elif command == "9":
+        if not products_sold:
+            print("Сьогодні ще нічого не продано")
+            input("Натисніть 'Enter' для продовження")
+            continue
+
+        print("Сьогодні продано такі товари:")
+        for i, product in enumerate(products_sold):
+            print(f"{i + 1}. {product}")
+
+        input("Натисніть 'Enter' для продовження")
+
+    elif command == "10":
+        if not products_sold:
+            print("Сьогодні ще нічого не продано")
+            input("Натисніть 'Enter' для продовження")
+            continue
+
+        products_temp = products_sold.copy()
+        products_temp.reverse()
+        print("Історія продажу товарів")
+        for product in products_temp:
+            print(product)
+
+        input("Натисніть 'Enter' для продовження")
+
+    elif command == "11":
+        print("Дякую що були з нами")
         break
-
-    prods = [prod[1] for prod in products]
-    if product not in prods:
-        products.append([len(products) + 1, product])
-    else:
-        print(f"Товар '{product}' вже наявний у списку")
-
-products.sort(key=lambda product: product[1])
-for product in products:
-    print(product)
-
