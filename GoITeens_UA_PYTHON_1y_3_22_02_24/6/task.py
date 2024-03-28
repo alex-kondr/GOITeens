ebc5513f716f2e521cacf354f332e3bb81b72e6c
Oleksandr Kondratiuk <48170652+alex-kondr@users.noreply.github.com>
2024-03-28 20:45:55 +0200

    save

--- GoITeens_UA_PYTHON_1y_3_22_02_24/6/task.py
+++ GoITeens_UA_PYTHON_1y_3_22_02_24/6/task.py
@@ -35,3 +35,63 @@ commands = [
     "10. Показати історію продажів",
     "11. Вийти з програми"
 ]
+
+while True:
+    print()
+    for command in commands:
+        print(command)
+
+    command = input("Введіть номер команди: ")
+
+    if command == "1":
+        for i in range(len(products)):
+            print(f"{i + 1}. {products[i]}")
+
+        input("\nНатисніть 'Enter' для продовження ")
+
+    elif command == "2":
+        product = input("Введіть новий товар для додавання до списку: ")
+
+        if product in products:
+            print("\nТакий товар вже є у списку")
+            input("\nНатисніть 'Enter' для продовження ")
+            continue
+
+        products.append(product)
+        print(f"Товар '{product}' доданий до списку")
+        input("\nНатисніть 'Enter' для продовження ")
+
+    elif command == "3":
+        prods = input("Введіть список товар для додавання через пробіл\n-> ")
+        products.extend(prods)
+        print("Список товарів розширено")
+        input("\nНатисніть 'Enter' для продовження ")
+
+    elif command == "4":
+        product = input("Введіть назву товару для видалення зі списку товарів: ")
+
+        if product not in products:
+            print("Такого товару немає у списку")
+            input("\nНатисніть 'Enter' для продовження ")
+            continue
+
+        products.remove(product)
+        print(f"Товар '{product}' видалено зі списку")
+        input("\nНатисніть 'Enter' для продовження ")
+
+
+# my_list1 = []
+# my_list2 = []
+# my_list1.extend(my_list2)
+# my_list1.sort()
+# print(my_list1)
+
+students = [["Андрій", "Іванов"], ["Андрій", "Нікітенко"], ["Адам", "Баранов"]]
+
+count = 0
+for student in students:
+    if student[0] == "Андрій":
+        count += 1
+
+print(count)
+# print(students.count(["Андрій", "Іванов"]))
