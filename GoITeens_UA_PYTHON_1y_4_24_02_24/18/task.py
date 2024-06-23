a01853c8987976cfe9d16533f32a334438f2265e
Oleksandr Kondratiuk <48170652+alex-kondr@users.noreply.github.com>
2024-06-23 11:37:33 +0300

    save

--- GoITeens_UA_PYTHON_1y_4_24_02_24/18/task.py
+++ GoITeens_UA_PYTHON_1y_4_24_02_24/18/task.py
@@ -1,5 +1,82 @@
+# import functions
+
+
+# result = functions.add(5, 9)
+
+# print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import matplotlib.pyplot as plt
+
+# import test_module
+
+
+
+# plt.plot(test_module.X, test_module.Y)
+# plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import test_module
+
+
+# print(test_module.X)
+# print(test_module.Y)
+
+
+# from test_module import X, Y
+
+
+# print(X)
+# print(Y)
+
+
+# from test_module import *
+
+
+# print(X)
+# print(Y)
+
+
+
+
+# print(random.randint(1, 6))
+
+
+# from random import *
+# import random
+
+
+# print(randint(1, 6))
+
+
+
 # import matplotlib.pyplot as plt
 # import numpy as np
+
 
 # x = np.linspace(0, 2, 100)
 
@@ -10,6 +87,7 @@
 # plt.ylabel('y label')
 # plt.title("Simple Plot")
 # plt.legend()
+# plt.show()
 
 
 
@@ -19,7 +97,11 @@
 # що приймає один аргумент msg та виводить його на екран.
 # Потім імпортувати функцію print_message() в головний файл програми та викликати її.
 
+# import my_module
 
+
+# my_module.print_message("Hello world")
+# my_module.print_message_end("Hello world")
 
 
 
@@ -40,9 +122,14 @@
 # та rectangle_area(width, height).
 # Імпортувати модуль та використати ці функції для обчислення площі кола та прямокутника.
 
+# import my_geometry as mg
 
 
+# circle_area = mg.circle_area(10)
+# print(f"Площа круга: {circle_area}")
 
+# rectangle_area = mg.rectangle_area(4.47951, 5.10226)
+# print(f"Площа прямокутника: {rectangle_area}")
 
 
 
@@ -50,7 +137,10 @@
 
 # Створити власний модуль my_data.py, який містить список даних (наприклад, список чисел).
 # Імпортувати модуль та вивести список даних на екран.
+# from my_data import my_data
 
+
+# print(f"main -> {my_data = }")
 
 
 
@@ -64,9 +154,15 @@
 # статистичних показників (наприклад, середнього значення, медіани, дисперсії) для списку даних.
 # Імпортувати модуль та використати ці функції для обробки списку даних.
 # D(X)= E(X^2) − μ^2
+# import my_statistics as ms
+# from my_data import my_data
 
 
+# avg = ms.evarage(my_data)
+# print(f"Середнє арифметичне: {avg}")
 
+# med = ms.median(my_data)
+# print(f"Медіана: {med}")
 
 
 
