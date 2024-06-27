debe418127d16f983b6494de4cc09b549ffa2b2a
Oleksandr Kondratiuk <48170652+alex-kondr@users.noreply.github.com>
2024-06-27 20:35:39 +0300

    save

--- GoITeens_UA_PYTHON_1y_3_22_02_24/19/task.py
+++ GoITeens_UA_PYTHON_1y_3_22_02_24/19/task.py
@@ -1,5 +1,71 @@
+# fh = open("hello", "r", encoding="utf-8")
+# fh = open("hello", "r+")
+# fh = open("hello", "w")
+# fh = open("hello", "w+")
+# fh = open("hello", "a")
+# fh = open("hello", "a+")
+
+
+# print(fh.read())
+# fh.write("Bye! World!")
+# print(fh.read())
+# fh.close()
+
+
+# fh = open("hello1.txt", "w", encoding="utf-8")
+# fh.write("Bye my World!")
+# fh.close()
+
+# fh = open("hello.txt")
+# file = fh.read()
+# fh.close()
+
+# print(file)
+
+# fh = open("hello.txt", "a+", encoding="utf-8")
+# file = fh.read()
+# print(file)
+# fh.writelines(["Другий рядок\n", "Третій рядок\n", "Четвертий рядок\n"])
+# fh.close()
+
+# import os
+
+
+# os.remove("hello1.txt")
+
+
+# with open("hello.txt", "w", encoding="utf-8") as fh:
+#     fh.write("Hello")
+#     fh.writelines(["Hello"])
+
+# with open("hello.txt", "r", encoding="utf-8") as fh:
+#     # print(fh.read())
+#     # print(fh.read(3))
+#     # print(fh.readline())
+#     # print(fh.readlines())
+#     fh.seek(3)
+#     fh.read()
+    
+
+
+
+
+
+
 # Напишіть програму, яка зчитує вміст файлу "input.txt"
 # і записує його у файл "output.txt" з виключенням повторюваних рядків.
+
+# with open("hello.txt", "r", encoding="utf-8") as fh:
+#     strings = fh.readlines()
+#     strings = set(strings)
+
+# with open("output.txt", "w", encoding="utf-8") as fh:
+#     fh.writelines(strings)
+
+
+
+# Реалізуйте програму, яка зчитує вміст файла "input.txt"
+# і зберігає його у файлі "output.txt" у зворотньому порядку.
 
 
 
@@ -9,9 +75,13 @@
 
 # Напишіть функцію, яка приймає шлях до файла як аргумент і повертає кількість рядків у файлі.
 
+# def len_lines(path: str) -> int:
+#     with open(path, "r", encoding="utf-8") as fh:
+#         return len(fh.readlines())
 
 
-
+# path = "hello.txt"
+# print(len_lines(path))
 
 
 
