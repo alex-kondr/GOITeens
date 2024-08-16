bec0d5d1e8520ca5bd30dbdf90f8b2753e08b123
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-08-16 19:32:02 +0300

    save

---
+++ GoITeens_UA_PYTHON_1y_10_06_04_24/18/main.py
@@ -0,0 +1,11 @@
+import my_module
+
+
+my_module.print_message("Hello world")
+
+from my_module import print_message, print_message_2, print_message_3
+
+from my_module import *
+
+
+print_message_3("Hello world")
---
+++ GoITeens_UA_PYTHON_1y_10_06_04_24/18/my_module.py
@@ -0,0 +1,10 @@
+def print_message(text: str) -> str:
+    print(text)
+
+
+def print_message_2(text: str) -> str:
+    print(text + "-> print-2")
+
+
+def print_message_3(text: str) -> str:
+    print(text + "-> print-3")
