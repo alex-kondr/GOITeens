8ff957af48d3e7614e00707ff98eb70c8fb4c9e2
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-08-16 15:31:46 +0300

    save

---
+++ GoITeens_UA_PYTHON_1y_10_06_04_24/18/task.py
@@ -0,0 +1,203 @@
+# import functions
+
+
+# result = functions.add(5, 9)
+
+# print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import matplotlib.pyplot as plt
+
+# import test_module
+
+
+
+# plt.plot(test_module.X, test_module.Y)
+# plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import test_module
+
+
+# print(test_module.X)
+# print(test_module.Y)
+
+
+# from test_module import X, Y
+
+
+# print(X)
+# print(Y)
+
+
+# from test_module import *
+
+
+# print(X)
+# print(Y)
+
+
+
+
+# print(random.randint(1, 6))
+
+
+# from random import *
+# import random
+
+
+# print(randint(1, 6))
+
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+
+# x = np.linspace(0, 2, 100)
+
+# plt.plot(x, x, label='linear')  # Plot some data on the (implicit) axes.
+# plt.plot(x, x**2, label='quadratic')  # etc.
+# plt.plot(x, x**3, label='cubic')
+# plt.xlabel('x label')
+# plt.ylabel('y label')
+# plt.title("Simple Plot")
+# plt.legend()
+# plt.show()
+
+
+
+
+
+# Створити власний модуль my_module.py, який містить функцію print_message(msg),
+# що приймає один аргумент msg та виводить його на екран.
+# Потім імпортувати функцію print_message() в головний файл програми та викликати її.
+
+# import my_module
+
+
+# my_module.print_message("Hello world")
+# my_module.print_message_end("Hello world")
+
+
+
+
+
+
+# Створити власний модуль my_math.py, який містить функції add(x, y) та subtract(x, y).
+# Імпортувати модуль та використати ці функції для додавання та віднімання чисел.
+
+
+
+
+
+
+
+
+# Створити власний модуль my_geometry.py, який містить функції circle_area(radius)
+# та rectangle_area(width, height).
+# Імпортувати модуль та використати ці функції для обчислення площі кола та прямокутника.
+
+# import my_geometry as mg
+
+
+# circle_area = mg.circle_area(10)
+# print(f"Площа круга: {circle_area}")
+
+# rectangle_area = mg.rectangle_area(4.47951, 5.10226)
+# print(f"Площа прямокутника: {rectangle_area}")
+
+
+
+
+
+# Створити власний модуль my_data.py, який містить список даних (наприклад, список чисел).
+# Імпортувати модуль та вивести список даних на екран.
+# from my_data import my_data
+
+
+# print(f"main -> {my_data = }")
+
+
+
+
+
+
+
+
+
+# Створити власний модуль my_statistics.py, який містить функції для обчислення
+# статистичних показників (наприклад, середнього значення, медіани, дисперсії) для списку даних.
+# Імпортувати модуль та використати ці функції для обробки списку даних.
+# D(X)= E(X^2) − μ^2
+# import my_statistics as ms
+# from my_data import my_data
+
+
+# avg = ms.evarage(my_data)
+# print(f"Середнє арифметичне: {avg}")
+
+# med = ms.median(my_data)
+# print(f"Медіана: {med}")
+
+
+
+
+
+
+# Створити графік лінійної функції y = 2x + 1 за допомогою бібліотеки matplotlib.
+
+
+
+
+
+
+
+# Створити графік квадратичної функції y = x^2 за допомогою бібліотеки matplotlib.
+
+
+
+
+
+
+
+# Створити гістограму для списку даних за допомогою бібліотеки matplotlib.
+
+
+
+
+
+# Створити діаграму розділу для списку даних за допомогою бібліотеки matplotlib.
+
+
+
+
+
+
+
+
+# Створити графік функції sin(x) за допомогою бібліотеки matplotlib.
