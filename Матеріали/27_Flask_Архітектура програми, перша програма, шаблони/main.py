7eafbf6f2de8198488d42b1189d90b7d0e3a3929
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-08-22 20:36:54 +0300

    save

---
+++ Матеріали/27_Flask_Архітектура програми, перша програма, шаблони/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+# @app.route("/")
+# def hello_world():
+#     return "Hello world!"
+
+
+@app.get("/")
+def index():
+    return render_template("index_pizza.html")
+
+
+@app.get("/menu/")
+def bootstrap_demo():
+    return render_template("menu.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
