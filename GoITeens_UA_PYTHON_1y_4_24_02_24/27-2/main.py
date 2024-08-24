1451323c042673fac56d73366708fdd27b472ab3
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-08-24 14:30:04 +0300

    save

--- GoITeens_UA_PYTHON_1y_4_24_02_24/27-2/main.py
+++ GoITeens_UA_PYTHON_1y_4_24_02_24/27-2/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 
 app = Flask(__name__)
@@ -6,12 +6,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index2.html")
 
 
 @app.route("/page_2/")
 def page_2():
     return render_template("page_2.html")
+
+
+@app.route("/2/")
+def redir():
+    return redirect("/")
 
 
 if __name__ == "__main__":
