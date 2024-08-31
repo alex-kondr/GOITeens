5d49ef7a2c5b5c2492319a34b0e4e35025cc0b4e
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-08-31 15:01:32 +0300

    save

--- Матеріали/28_Flask. GET, POST запити. Шаблонізатор Jinija/pizza/main.py
+++ Матеріали/28_Flask. GET, POST запити. Шаблонізатор Jinija/pizza/main.py
@@ -1,9 +1,9 @@
 import random
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, request, url_for, abort
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 count = 0
 
@@ -59,5 +59,54 @@ def testr():
     return render_template("test.html", title="Hello")
 
 
+@app.get("/api/results/")
+def api_results():
+    context = {
+        "title": "Results",
+        "students": students,
+        "test_name": test_name,
+        "max_score": max_score,
+    }
+    return context
+
+
+@app.get("/login/")
+@app.post("/login/")
+def login():
+    name = request.form.get("name")
+    print(request.form)
+    return render_template("login.html", name=name)
+
+
+@app.get("/url_test/")
+def url_test():
+    return url_for("login")
+
+
+@app.get("/url/")
+def test_url():
+    abort(401)
+    return "Доступ заборонено"
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    print(f"{error = }")
+    print(request.base_url)
+    return "error", 404
+
+
+@app.get("/test_index/")
+def test_index():
+    return render_template("test_index.html")
+
+
 if __name__ == "__main__":
+    with app.test_request_context():
+        print(url_for('index'))
+        print(url_for('contacts'))
+        print(url_for('menu'))
+        print(url_for('login'))
+        print(url_for('results'))
+
     app.run(debug=True, port=1234)
