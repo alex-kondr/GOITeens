02e42ec1ace7a4244f3dc6f3ba96689c4e3b3d60
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-09-07 11:34:14 +0300

    save

---
+++ Матеріали/29_Flask_меню навігації. Застосування фільтрів. Сортування. Макроси/my_pizza/main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def index():
+    return render_template("index.html", title="Моя супер піцерія")
+
+
+@app.get("/menu-ababagalamaga/")
+def menu():
+    pizzas = [
+        {"name": "Пепероні", "price": 22, "ingredients": "ковбаса 'Пепероні', сир, соус"},
+        {"name": "Пепероні", "price": 29, "ingredients": "ковбаса 'Пепероні', сир, соус"},
+        {"name": "Моцарела", "price": 20, "ingredients": "сир 'Моцарела', соус, петрушка"},
+        {"name": "Чотири сира", "price": 30, "ingredients": "сир 'Моцарела', сир 'Чедер', сир 'Сологуні', сир 'Брю'"}
+    ]
+    context = {
+        "pizzas": pizzas,
+        "title": "Мега меню"
+    }
+    return render_template("menu.html", **context)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
