fa11fe379253edb8840d3666ab0869854016feec
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-09-18 18:29:28 +0300

    save

--- Матеріали/31_Flask_SQLAlchemy/app/routes/employee.py
+++ Матеріали/31_Flask_SQLAlchemy/app/routes/employee.py
@@ -1,9 +1,27 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
+
+from app import Session
+from app.models import Employee
 
 
 employees_blueprint = Blueprint("employees", __name__)
 
 
 @employees_blueprint.get("/")
-def index():
-    return render_template("index.html", title="Головна")
+@employees_blueprint.post("/")
+def add_employee():
+    with Session() as session:
+        if request.method == "POST":
+            employee = Employee(**request.form)
+            session.add(employee)
+            session.commit()
+
+        employees = session.query(Employee).all()
+
+    return render_template("employee/management.html", employees=employees)
+
+
+@employees_blueprint.get("/<int:id>/")
+def get_employee(id):
+    with Session() as session:
+        pass
--- Матеріали/31_Flask_SQLAlchemy/app/routes/position.py
+++ Матеріали/31_Flask_SQLAlchemy/app/routes/position.py
@@ -21,9 +21,8 @@ def add_position():
     return render_template("position/management.html", positions=positions)
 
 
-@positions_blueprint.get("/<int:id>")
+@positions_blueprint.get("/<int:id>/")
 def get_positions(id):
     with Session() as session:
-        position = session.scalars(select(Position).where(Position.id == id)).first()
-        print(position)
-        return render_template("index.html", position=position)
+        content = session.scalars(select(Position).where(Position.id == id)).first()
+        return render_template("index.html", content=content, title="Інформація про посаду")
