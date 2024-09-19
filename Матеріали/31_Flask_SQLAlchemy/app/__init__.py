802c2b73cacd885ce02cd38fdf9d515789b149da
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-09-19 16:32:09 +0300

    save

--- Матеріали/31_Flask_SQLAlchemy/app/__init__.py
+++ Матеріали/31_Flask_SQLAlchemy/app/__init__.py
@@ -3,12 +3,12 @@ from flask import Flask
 from app.routes.employee import employees_blueprint
 from app.routes.position import positions_blueprint
 from app.routes.cabinet import cabinets_blueprint
+from app.routes.root import root_blueprint
 from app.models.base import create_db
 from app.models.associates import cabinet_position_assoc_table, employee_position_assoc_table
 from app.models.cabinet import Cabinet
 from app.models.employee import Employee
 from app.models.position import Position
-from app.routes.root import root_blueprint
 
 
 app = Flask(__name__)
