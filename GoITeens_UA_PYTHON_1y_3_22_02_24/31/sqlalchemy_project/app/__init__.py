a59f36a85b116bb8f31bb709500060fdb8266952
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-09-24 23:22:49 +0300

    save

--- GoITeens_UA_PYTHON_1y_3_22_02_24/31/sqlalchemy_project/app/__init__.py
+++ GoITeens_UA_PYTHON_1y_3_22_02_24/31/sqlalchemy_project/app/__init__.py
@@ -4,9 +4,11 @@ from app.models.base import create_db
 from app.models.employee import Employee
 from app.models.position import Position
 from app.routes.position import position_blueprint
+from app.routes.employee import employee_blueprint
 
 
-app = Flask(__name__, static_folder="app/static", template_folder="app/templates")
+app = Flask(__name__)
 app.register_blueprint(position_blueprint)
+app.register_blueprint(employee_blueprint)
 
 create_db()
