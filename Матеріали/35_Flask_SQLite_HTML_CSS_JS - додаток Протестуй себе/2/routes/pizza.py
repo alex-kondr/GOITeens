93584daa344d911d00c48fa0d19f0620cf2e3aea
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2024-10-17 14:44:08 +0300

    save

---
+++ Матеріали/35_Flask_SQLite_HTML_CSS_JS - додаток Протестуй себе/2/routes/pizza.py
@@ -0,0 +1,106 @@
+from tkinter import SE
+from flask import Blueprint, render_template, request, redirect, url_for
+
+from models.base import Session
+from models.pizza import Pizza
+from models.ingredient import Ingredient
+from data.wheather import get_wheather
+
+
+pizza_route = Blueprint("pizzas", __name__)
+
+
+@pizza_route.get("/")
+def index():
+    wheather = get_wheather("Neratovice")
+
+    if 40 > wheather.get("temp") > 12:
+        pizza_name = "Тепла"
+    elif wheather.get("temp") <= 12:
+        pizza_name = "Холодна"
+    elif wheather.get("temp") > 40:
+        pizza_name = "Пепероні"
+
+    return render_template("index.html", title="Суперова піцерія", wheather=wheather, pizza_name=pizza_name)
+
+
+@pizza_route.get("/menu/")
+def menu():
+    wheather = get_wheather("Kharkiv")
+    with Session() as session:
+        pizzas = session.query(Pizza).all()
+        ingredients = session.query(Ingredient).all()
+
+        context = {
+            "pizzas": pizzas,
+            "ingredients": ingredients,
+            "title": "Шикайне меню",
+            "wheather": wheather
+        }
+        return render_template("menu.html", **context)
+
+
+@pizza_route.post("/add_pizza/")
+def add_pizza():
+    with Session() as session:
+        name = request.form.get("name")
+        price = request.form.get("price")
+
+        ingredients = request.form.getlist("ingredients")
+        ingredients = session.query(Ingredient).where(Ingredient.id.in_(ingredients)).all()
+
+        pizza = Pizza(name=name, price=price, ingredients=ingredients)
+        session.add(pizza)
+        session.commit()
+        return redirect("/menu/")
+
+
+@pizza_route.get("/pizza/del/<int:id>>/")
+def del_pizza(id):
+    with Session() as session:
+        pizza = session.query(Pizza).where(Pizza.id == id).first()
+        session.delete(pizza)
+        session.commit()
+        return redirect(url_for("pizzas.menu"))
+
+
+@pizza_route.get("/pizza/edit/<int:id>")
+@pizza_route.post("/pizza/edit/<int:id>")
+def edit_pizza(id):
+    with Session() as session:
+        pizza = session.query(Pizza).where(Pizza.id == id).first()
+        if request.method == "POST":
+            name = request.form.get("name")
+            price = request.form.get("price")
+
+            pizza.name = name
+            pizza.price = price
+            session.commit()
+            return redirect(url_for("pizzas.menu"))
+
+        return render_template("edit_pizza.html", pizza=pizza)
+
+
+@pizza_route.get("/poll/")
+def poll():
+    with Session() as session:
+        pizzas = session.query(Pizza).all()
+        question = "Яка піца вам сподобалась найбільше"
+        return render_template("poll.html", question=question, pizzas=pizzas)
+
+
+@pizza_route.get("/add_answer/")
+def add_answer():
+    answer = request.args.get("answer")
+    with open("data/answers.txt", "a", encoding="utf-8") as file:
+        file.write(answer + "\n")
+
+    return redirect(url_for('pizzas.answers'))
+
+
+@pizza_route.get("/answers/")
+def answers():
+    with open("data/answers.txt", "r", encoding="utf-8") as file:
+        data = file.readlines()
+
+    return render_template("answers.html", answers=data)
