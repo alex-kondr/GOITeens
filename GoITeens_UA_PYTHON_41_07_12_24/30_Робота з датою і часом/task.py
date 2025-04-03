b8c4f59505ada3fd6594c64bc2b1f43e344e0edf
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2025-04-03 13:39:32 +0300

    save

---
+++ GoITeens_UA_PYTHON_41_07_12_24/30_Робота з датою і часом/task.py
@@ -0,0 +1,57 @@
+#  len()
+
+
+
+
+# max(), min()
+
+
+
+
+
+# abs()
+
+
+
+
+# sum()
+
+
+
+# datetime, strftime, fromtimestamp, locale
+# https://acode.com.ua/method-strftime-python/
+# from datetime import date, time, datetime, timedelta
+# import locale
+
+
+
+
+# time
+# import time
+
+# print(time.gmtime())
+# print(time.localtime())
+
+
+# Розробіть скрипт, який запитує у користувача
+# дві дати (у форматі рррр-мм-дд) і
+# обчислює різницю між ними в днях.
+
+
+
+
+
+
+
+# math, sqrt, log, ceil, floor, sin, asin, degrees
+
+
+
+
+
+
+# os, sys, random
+# import os
+# os.chdir("C:\\Users\\user\\Documents")
+# print(os.getcwd())
+# print(os.listdir())
