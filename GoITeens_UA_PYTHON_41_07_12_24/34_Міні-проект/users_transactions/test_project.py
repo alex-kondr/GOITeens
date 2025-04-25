685c998a557c8cf7497860963031904a71037ec8
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2025-04-25 14:30:33 +0300

    save

--- GoITeens_UA_PYTHON_41_07_12_24/34_Міні-проект/users_transactions/test_project.py
+++ GoITeens_UA_PYTHON_41_07_12_24/34_Міні-проект/users_transactions/test_project.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 import os
 import json
 
@@ -12,28 +12,28 @@ from users import save_users, sign_up, get_users, login_user
 
 # def mock_get_users(file: str = "mock_users_db.json"):
 #     return get_users(file)
-
+mock = MagicMock(return_value=[])
 
 class TestStringMethods(unittest.TestCase):
 
-    def setUp(self):
-        with open("mock_users_db.json", "w", encoding="utf-8") as fd:
-            json.dump([], fd, ensure_ascii=False, indent=2)
+    # def setUp(self):
+    #     with open("mock_users_db.json", "w", encoding="utf-8") as fd:
+    #         json.dump([], fd, ensure_ascii=False, indent=2)
 
     # def tearDown(self):
     #     os.remove("mock_users_db.json")
 
-    @patch("users.save_users", lambda users: save_users(users, "mock_users_db.json"))
     # @patch("users.save_users", mock_save_users)
     # @patch("users.get_users", mock_get_users)
-    @patch("users.get_users", lambda: get_users("mock_users_db.json"))
+    @patch("users.save_users", MagicMock())
+    @patch("users.get_users", MagicMock(return_value=[]))
     def test_sign_up(self):
         self.assertTrue(sign_up("login", "password", "name"))
 
-    @patch("users.save_users", lambda users: save_users(users, "mock_users_db.json"))
     # @patch("users.save_users", mock_save_users)
     # @patch("users.get_users", mock_get_users)
-    @patch("users.get_users", lambda: get_users("mock_users_db.json"))
+    @patch("users.save_users", MagicMock())
+    @patch("users.get_users", mock)
     def test_login_user(self):
         sign_up("login", "password", "name")
         self.assertIsNotNone(login_user("login", "password"))
@@ -44,3 +44,7 @@ class TestStringMethods(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+# mock = MagicMock(return_value=[])
+# mock.return_value = 
+# print(mock())
